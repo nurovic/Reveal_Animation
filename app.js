cb007e9eefd42fdb1341466210e91df5d4cadae8
Mert <85342773+nurovic@users.noreply.github.com>
2022-06-21 23:50:44 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.front-page',
+        start: '0%',
+        end: "100%",
+        scrub: true,     
+        pin: true,
+    }
+})
+
+tl.fromTo('.front-page', 
+{clipPath: 'circle(5%)'},
+{clipPath: 'circle(75%)', duration: 3}
+)
+
+tl.fromTo('music-note', {scale: 0.5}, {scale: 0, opacity: 0 , duration: 1}, "-=3")
+
+tl.fromTo('.title', {opacity: 0}, {opacity: 1, duration: 1})
+tl.fromTo('.sub-title', {opacity: 0}, {opacity: 1, duration: 1})
+
+let img = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.second-page',
+        start: '0%',
+        end: "100%",
+        scrub: true,     
+        pin: true,
+    }
+})
+
+img.fromTo('.img-1',
+{opacity: 0}, {clipPath:"circle(75%)", opacity: 1})
+
+img.fromTo('.img-2',
+{opacity: 0 }, {clipPath:"circle(75%)", opacity: 1})
